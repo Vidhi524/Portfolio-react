e2f269bd21a908dffbeb5f284f8c94a5647f02b1
Vidhi524 <avidhi940@gmail.com>
2024-06-27 07:18:02 +0530

    protfolio

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <img src="" alt="" />
+      <h1>Hi! I'm Vidhi Arora, frontend developer based in Delhi, India.</h1>
+      <p>I </p>
+      <div className="hero-action">
+        <div className="hero-connect"></div>
+        <div className="hero-resume"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
