0a00dc77e3bf0239b3af5ada422356d849b397d1
Vidhi524 <avidhi940@gmail.com>
2024-06-28 21:15:18 +0530

    modified

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import './Hero.css'
+import myPic2 from '../../assets/picV2.jpeg'
 
 const Hero = () => {
   return (
     <div className='hero'>
-      <img src="" alt="" />
-      <h1>Hi! I'm Vidhi Arora, frontend developer based in Delhi, India.</h1>
-      <p>I </p>
+      <img src={myPic2} className='myPic1' alt="" />
+      <h1><span>Hi! I'm Vidhi Arora,</span> frontend developer based in Delhi, India.</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit officia quos blanditiis fuga impedit reiciendis, illum vero quo maxime sunt! Rem aspernatur, cupiditate delectus iste in recusandae facilis mollitia hic.</p>
       <div className="hero-action">
-        <div className="hero-connect"></div>
-        <div className="hero-resume"></div>
+        <div className="hero-connect">Connect with me</div>
+        <div className="hero-resume">My Resume</div>
       </div>
     </div>
   )
